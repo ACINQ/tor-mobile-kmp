bf1056eb78118188a09db3fedcfa5750aa8a82de
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2024-01-25 16:19:25 +0100

    Use secp256k1 0.13.0
    This tag supports kotlin 1.9.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,5 +3,5 @@ object Versions {
     const val dokka = "1.9.10"
     const val ndk = "21.3.6528147"
     const val ktor = "2.3.7"
-    const val secp256k1 = "0.13.0-SNAPSHOT"
+    const val secp256k1 = "0.13.0"
 }
